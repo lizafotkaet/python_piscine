b6f908a0a8555b4cbfc9273da68336aedb08d367
Elizaveta Barbash <ebarbash@f0r4s17.codam.nl>
2026-04-20 20:14:35 +0200

    module 4 ex04

--- module4/ex03/float.py
+++ module4/ex03/float.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
 
-num = float(input("Enter a number"))
+num = float(input("Enter a number: "))
+
+test = int(num)
+
+# if (num == test):
+# 	print("Number is an integer")
+# else:
+# 	print("Number is a decimal")
 
 if num.is_integer():
-	
+	print("Num is an int")
+else:
+	print("Num is a decimal")
---
+++ module4/ex04/round_up.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+num = float(input("Enter a number: "))
+
+result = int()
